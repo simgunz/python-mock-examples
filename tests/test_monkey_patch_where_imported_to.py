7b6b83be293c333c731e244694d21e00b9fb5a73
Simone Gaiarin <simgunz@gmail.com>
2022-04-02 08:54:39 +0200

    Fix typo

--- tests/test_monkey_patch_where_imported_to.py
+++ tests/test_monkey_patch_where_imported_to.py
@@ -11,7 +11,7 @@ def test_monkey():
 
 @mock.patch("mockexamples.external_api_banana.banana")
 def test_monkey_mock_wrong(mock_banana):
-    """This tests is red because I am patching where the object is imported from."""
+    """This test is red because I am patching where the object is imported from."""
     mock_banana.return_value = "mango"
 
     assert monkey.eat_banana() == "monkey eats mango"
@@ -19,14 +19,14 @@ def test_monkey_mock_wrong(mock_banana):
 
 @mock.patch("mockexamples.monkey.banana")
 def test_monkey_mock_correct(mock_banana):
-    """This tests is green because I am patching where the object is imported to."""
+    """This test is green because I am patching where the object is imported to."""
     mock_banana.return_value = "mango"
 
     assert monkey.eat_banana() == "monkey eats mango"
 
 
 def test_monkey_mocker_correct(mocker):
-    """This tests shows the alternative syntax using the mocker fixture (pytest-mock)."""
+    """This test shows the alternative syntax using the mocker fixture (pytest-mock)."""
     mock_banana = mocker.patch("mockexamples.monkey.banana")
     mock_banana.return_value = "mango"
 
